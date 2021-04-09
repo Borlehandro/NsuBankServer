107dadca6786bad98b9b4ad52fe08c0deb7195f0
brainworm <borlehandro@gmail.com>
2021-04-09 19:12:18 +0700

    Create server template from Spring inittializr

---
+++ src/main/java/com/sibdever/nsu_bank_system_server/NsuBankSystemServerApplication.java
@@ -0,0 +1,13 @@
+package com.sibdever.nsu_bank_system_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsuBankSystemServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NsuBankSystemServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sibdever/nsu_bank_system_server/NsuBankSystemServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sibdever.nsu_bank_system_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NsuBankSystemServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
